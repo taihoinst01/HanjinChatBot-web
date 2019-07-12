60e8a7c8eca0e0fb076978e647a4ade012bab909
junhpark <junhpark@taihoinst.com>
2019-07-12 14:25:14 +0900

    창 닫기

--- webapps/ROOT/assets/js/start.js
+++ webapps/ROOT/assets/js/start.js
@@ -25,6 +25,21 @@ function isMobile() {
     }
 }
 
+function closeWin() {
+
+    var nvua = navigator.userAgent;
+    if (nvua.indexOf('MSIE') >= 0) {
+        if (nvua.indexOf('MSIE 5.0') == -1) {
+            top.opener = '';
+        }
+    } else if (nvua.indexOf('Gecko') >= 0) {
+        top.name = 'CLOSE_WINDOW';
+        wid = window.open('', 'CLOSE_WINDOW');
+    }
+    top.close();
+}
+
+
 if (isMobile()) {   //모바일 환경
     //location.href = "default_m.asp";   
 }
